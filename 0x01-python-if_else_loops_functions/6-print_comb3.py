84caceafb41c99501969e2fd69e4028a233674fa
kambidy <ryanmulwa100@gmail.com>
2023-06-06 19:57:21 +0300

    learining-py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for digit2 in range(digit1 + 1, 10):
+    if digit1 == 8 and digit2 == 9:
+        print("{}{}".format(digit1, digit2))
+    else:
+        print("{}{}".format(digit1, digit2), end=", ")
