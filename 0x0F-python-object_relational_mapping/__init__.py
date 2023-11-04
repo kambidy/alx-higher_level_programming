96d597c1eeccf97ec8472a632148f06fafa3bb2f
kambidy <ryanmulwa100@gmail.com>
2023-11-04 22:22:24 +0300

    learning-py

--- 0x0F-python-object_relational_mapping/__init__.py
+++
@@ -1,6 +0,0 @@
-from model_state import State
-from model_city import City
-
-
-state = State()
-city = City()
--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const fun = 'C is fun';
-const thon = 'Python is cool';
-const javas = 'JavaScript is amazing';
-console.log(fun);
-console.log(thon);
-console.log(javas);
