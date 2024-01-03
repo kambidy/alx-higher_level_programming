bdaa02ccd5c52ec193d00908d1dc71700ac7d8d0
kambidy <ryanmulwa100@gmail.com>
2024-01-03 15:48:06 +0300

    scrape

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+const { argv } = require('process');
 
-request(url, function (err, response, body) {
-	if (err) {
-		console.log(err);
-	}
-	console.log(JSON.parse(body).title);
+const BaseUrl = 'https://swapi-api.alx-tools.com/api';
+request(BaseUrl + '/films/' + argv[2], (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  console.log(JSON.parse(body).title);
 });
